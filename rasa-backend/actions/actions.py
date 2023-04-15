73fcff8eb2d4ce7a1192c46513da58bd4d286cc3
Bharathkumarkaar <bharathkumarkaar@gmail.com>
2023-04-15 18:02:33 +0530

    code done for all api

--- rasa-backend/actions/actions.py
+++ rasa-backend/actions/actions.py
@@ -31,7 +31,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from actions.api import prlist,pritems
+from actions.api import prlist,pritems,pritemdetails
 
 class ActionHelloWorld(Action):
 
@@ -58,7 +58,7 @@ class ActionPRList(Action):
         prlists = prlist()
         prlists = prlists[:10]
         # dispatcher.utter_template("utter_givepr",tracker,temp=prlists)
-        message = f"The list of PR's are: {prlists}, Choose a PR Number to display its items"
+        message = f"The list of PR's are: {prlists}. Choose a PR Number to display its items"
         dispatcher.utter_message(text=message)
 
         return []
@@ -77,6 +77,44 @@ class ActionPRitems(Action):
         pritemslist = pritemslist[:10]
         # dispatcher.utter_template("utter_givepr",tracker,temp=prlists)
         message = f"The list of PR's items are: {pritemslist}"
+        dispatcher.utter_message(text=message)
+
+        return []
+    
+class ActionPRitemDesc(Action):
+
+    def name(self) -> Text:
+        return "action_pr_item_desc"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # prno=tracker.latest_message['text']
+        prno='10000640'
+        pritemno='220'
+        pritemdesc = pritemdetails(prno,pritemno)
+        for i in pritemdesc.keys():
+            if i=='Requisitioner_Name':
+                Name = pritemdesc[i]
+            elif i=='Purchase_Requisition_Number':
+                PRnumber = pritemdesc[i]
+            elif i=='Purchase_Requisition_Item_Number':
+                PRItemNumber = pritemdesc[i]
+            elif i=='Purchase_Requisition_Material':
+                Material = pritemdesc[i]
+            elif i=='Purchase_Requisition_Item_Text':
+                PRItemText = pritemdesc[i]
+            elif i=='Purchase_Requisition_Release_Date':
+                PRReleaseDate = pritemdesc[i]
+        # dispatcher.utter_template("utter_givepr",tracker,temp=prlists)
+        new_line='\n'
+        message = f"""Here are the Details of Purchase Requisition... {new_line} 
+        Requisitioner Name : {Name} {new_line}
+        Purchase Requisition Number : {PRnumber} {new_line}
+        Purchase Requisition Item Number : {PRItemNumber} {new_line}
+        Purchase Requisition Material : {Material} {new_line}
+        Purchase Requisition Item Text : {PRItemText} {new_line}
+        Purchase_Requisition_Release_Date : {PRReleaseDate}"""
         dispatcher.utter_message(text=message)
 
         return []
--- rasa-backend/actions/api.py
+++ rasa-backend/actions/api.py
@@ -46,18 +46,26 @@ def pritems(prno):
             break
     return itemlist
 
+def pritemdetails(prno,pritemno):
+    api_address=f'http://172.26.0.121:8002/sap/opu/odata/sap/API_PURCHASEREQ_PROCESS_SRV/A_PurchaseRequisitionItem(PurchaseRequisition=\'{prno}\',PurchaseRequisitionItem=\'{pritemno}\')'
+    url = api_address
+    response = session.get(url)
+# Print the response status code and content
+    obj = response.content
+    objstr = str(obj, 'UTF-8')
+    obj2 = xmltodict.parse(objstr)
+    js = json.dumps(obj2)
+    js_obj = json.loads(js)
+    flatjs = flatten(js_obj)
+    desc={}
+    desc['Requisitioner_Name'] = flatjs['entry_content_m:properties_d:RequisitionerName']
+    desc['Purchase_Requisition_Number'] = flatjs['entry_content_m:properties_d:PurchaseRequisition']
+    desc['Purchase_Requisition_Item_Number'] = flatjs['entry_content_m:properties_d:PurchaseRequisitionItem']
+    desc['Purchase_Requisition_Material'] = flatjs['entry_content_m:properties_d:Material']
+    desc['Purchase_Requisition_Item_Text'] = flatjs['entry_content_m:properties_d:PurchaseRequisitionItemText']
+    desc['Purchase_Requisition_Release_Date'] = flatjs['entry_content_m:properties_d:PurchaseRequisitionReleaseDate'] 
+    return desc
 
-
-# def prupdate(prno):
-#     api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
-#     # city = input('Enter the City Name :')
-#     url = api_address + prno
-#     json_data = requests.get(url).json()
-#     format_add = json_data['main']
-#     print(format_add)
-#     # print("Weather is {0} Temperature is mininum {1} Celcius and maximum is {2} Celcius".format(
-#     #     json_data['weather'][0]['main'],int(format_add['temp_min']-273),int(format_add['temp_max']-272)))
-#     return format_add
 
 # def prdelete(prno):
 #     api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
